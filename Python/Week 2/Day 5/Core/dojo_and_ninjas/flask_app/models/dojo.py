37cede12d79861dd4925f92e78cc6be34664a38d
koukisaifedinn <koukisaif1996@gmail.com>
2024-03-04 16:35:24 +0100

    flask-mysql

---
+++ Python/Week 2/Day 5/Core/dojo_and_ninjas/flask_app/models/dojo.py
@@ -0,0 +1,50 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.ninja import Ninja
+
+class Dojo:
+    def __init__(self,data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.ninjas = []
+
+
+
+
+
+    @classmethod
+    def read_all_dojos(cls):
+        query = "SELECT * FROM dojo ORDER BY id DESC"
+        db_dojo = connectToMySQL("dojoANDninjas_ass").query_db(query)
+        dojos = []
+        for dojo in db_dojo:
+            dojos.append(cls(dojo))
+        
+        return dojos
+
+
+
+
+
+
+    @classmethod
+    def create_dojo(cls,data):
+        query = "INSERT INTO dojo (name) VALUES (%(name)s)"
+        return connectToMySQL("dojoANDninjas_ass").query_db(query,data)
+
+
+
+
+
+    @classmethod
+    def read_dojo(cls,data):
+        query = "SELECT * FROM dojo WHERE id = %(id)s"
+        read_dojo = connectToMySQL("dojoANDninjas_ass").query_db(query,data)
+        query = "SELECT * FROM ninjas WHERE dojo_id = %(id)s"
+        ninjas = connectToMySQL("dojoANDninjas_ass").query_db(query,data)
+        dojo = cls(read_dojo[0])
+        for ninja in ninjas:
+            dojo.ninjas.append(Ninja(ninja))
+        return dojo
+
