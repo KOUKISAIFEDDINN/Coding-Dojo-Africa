a33d529b8a7ed310ed760a27d539c1e30eefc3a1
koukisaifedinn <koukisaif1996@gmail.com>
2024-04-12 14:56:11 +0100

    mearn

--- MERN/Week 1/Day 5/Core/tabels-react/src/components/tabel-display.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Content = (props) => {
-    return <div>Tab {props.content} content is showing here</div>;
-};
-
-export default Content;
--- MERN/Week 1/Day 5/Practice/boxgenerator/src/components/colordisplay.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-const Color = (props) => {
-    return (
-        <div className="container" style={{ backgroundColor: props.boxColor, width: "50px", height: "50px" }}></div>)
-}
-export default Color
